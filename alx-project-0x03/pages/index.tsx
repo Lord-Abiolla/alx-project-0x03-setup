f3921df4905f5dbccc947e079b93441f2bf52190
Lord-Abiolla <abiollajames@protonmail.com>
2025-11-20 23:00:52 +0300

    Setting up a landing page with imperative routing

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-const router = useRouter();
+// const router = useRouter();
 
 interface PageRouteProps {
   pageRoute: string
